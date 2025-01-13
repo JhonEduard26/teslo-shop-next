963ed0af4fb77edb94007c6586cbdc83804e81a8
jhon <jhon.bocanegra26@gmail.com>
2025-01-13 13:05:40 -0500

    Add authentication and shop layouts with login and new account pages

---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,9 @@
+import { titleFont } from "@/config/fonts";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={`${titleFont.className}`}>New account page</h1>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body
-        className={``}
+        className={`antialiased`}
       >
         {children}
       </body>
