0ded18a25d4d524cb4b208df3f65c04cff94b78f
jhon <jhon.bocanegra26@gmail.com>
2025-01-13 12:45:13 -0500

    Refactor layout and typography: update language to Spanish, remove unused fonts, and introduce Montserrat for headings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { titleFont } from "@/config/fonts";
+
 export default function Home() {
   return (
     <div className="">
-      <h1>Hola Next</h1>
+      <h1 className="text-4xl">Hola Next</h1>
+      <h1 className={`${titleFont.className} text-4xl font-bold antialiased`}>Hola Montserrat</h1>
     </div>
   );
 }
