58d8743e2a4a581f2bf73fcd212ebba871935ea1
jhon <jhon.bocanegra26@gmail.com>
2025-01-13 18:14:52 -0500

    Remove old shop layout and pages; add new structure for shop, cart, checkout, and admin sections

---
+++ src/app/(shop)/cart/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h2>Cart page</h2>
+    </div>
+  )
+}
---
+++ src/app/(shop)/orders/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h2>Listado de ordenes</h2>
+    </div>
+  )
+}
